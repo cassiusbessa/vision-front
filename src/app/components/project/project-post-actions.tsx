faeb268153f5125958a03cbc9b76ad4485598dc1
cassius <cassiusbessa@gmail.com>
2024-03-10 04:40:52 -0300

    refactor: :art: change icons sizes

--- src/app/components/project/project-post-actions.tsx
+++ src/app/components/project/project-post-actions.tsx
@@ -7,15 +7,15 @@ export default function ProjectPostActions() {
   return (
     <div className="card-links flex items-center justify-evenly">
       <button type="button" className="btn btn-square btn-ghost w-fit h-fit p-1">
-        <CiHeart className="md:w-8 md:h-8 w-6 h-6" />
+        <CiHeart className="md:w-6 md:h-6 w-8 h-8" />
         <span className="hidden md:block">Curtir</span>
       </button>
       <button type="button" className="btn btn-square btn-ghost w-fit h-fit p-1">
-        <FaRegComment className="md:w-8 md:h-8 w-6 h-6" />
+        <FaRegComment className="md:w-6 md:h-6 w-8 h-8" />
         <span className="hidden md:block">Comentar</span>
       </button>
       <button type="button" className="btn btn-square btn-ghost w-fit h-fit p-1">
-        <GrSend className="md:w-8 md:h-8 w-6 h-6" />
+        <GrSend className="md:w-6 md:h-6 w-8 h-8" />
         <span className="hidden md:block">Enviar</span>
       </button>
     </div>
