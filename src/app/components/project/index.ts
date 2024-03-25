31d65040accd3d6951a6a439cec969718a428dbe
cassius <cassiusbessa@gmail.com>
2024-03-24 22:42:24 -0300

    refactor: :art: resize project post container and remove project post card margin

--- src/app/components/project/index.ts
+++ src/app/components/project/index.ts
@@ -1,5 +1,9 @@
 import ProjectCard from './project-card';
 import StarProject from './start-project';
 import ProjectProfileContainer from './project-profile-container';
+import ProjectPostCard from './project-post-card';
+import ProjectPostContainer from './project-post-container';
 
-export { ProjectCard, StarProject, ProjectProfileContainer };
+export {
+  ProjectCard, StarProject, ProjectProfileContainer, ProjectPostCard, ProjectPostContainer,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/header';
 import { projectsMock } from './mocks';
-import ProjectPostContainer from './components/project/project-post-container';
+import { ProjectPostContainer } from './components/project';
 
 export default function Home() {
   return (
     <div className="bg-base-200 flex flex-col items-center min-h-screen h-screen overflow-y-auto">
       <Header />
+      <label htmlFor="post-project" className="input input-bordered flex items-center gap-2 mt-6 w-3/4 md:w-1/2">
+        <span className="text-xl sr-only">🚀</span>
+        <input id="post-project" type="text" className="grow" placeholder="Compartilhe seu projeto!" />
+      </label>
       <ProjectPostContainer projectsInfo={projectsMock} />
     </div>
   );
